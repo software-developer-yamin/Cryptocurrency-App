ac93335adca596e8a0173369b1e75d3cb9de27c8
DeveloperYamin <developer.yamin@gmail.com>
2022-02-26 00:53:25 +0600

    customize navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Avatar, Typography } from "antd";
+import { Link } from "react-router-dom";
+import Icon from "../images/cryptocurrency.png";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="nav-container">
+      <div className="nav-container">
+        <Avatar src={Icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptocurrency</Link>
+        </Typography.Title>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
