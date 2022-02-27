39555ceac3b13f57bb3b2bef571e98acbfa28a0d
DeveloperYamin <developer.yamin@gmail.com>
2022-02-27 21:02:09 +0600

    Cryptocurrencies component done!

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ import News from "./News";
 const { Title } = Typography;
 
 function Home() {
-  const {data, isFetching } = useGetCryptosQuery();
+  const {data, isFetching } = useGetCryptosQuery(10);
   console.log(data);
   const globalStats = data?.data?.stats;
   if(isFetching) return "Loading...";
