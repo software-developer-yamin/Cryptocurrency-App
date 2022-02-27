aaa2956fe311402edb612d5fb826b74ac8764e39
DeveloperYamin <developer.yamin@gmail.com>
2022-02-27 21:53:47 +0600

    add img alt text

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -45,7 +45,7 @@ const Cryptocurrencies = ({ simplified }) => {
             <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
-                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                extra={<img className="crypto-image" src={currency.iconUrl} alt={currency.name} />}
                 hoverable
               >
                 <p>Price: {millify(currency.price)}</p>
