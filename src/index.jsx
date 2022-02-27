b6cc311302a132a9627965a8c27828228b1021a9
DeveloperYamin <developer.yamin@gmail.com>
2022-02-27 17:36:57 +0600

    create redux store component

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,18 @@
+import "antd/dist/antd.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import store from "./app/store";
 import reportWebVitals from "./reportWebVitals";
-import "antd/dist/antd.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
